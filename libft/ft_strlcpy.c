4acaca7acca2cd70ddec4f70736bab6b8ddb1760
Ramzi Fkaier <rfkaier@c2r7p8.42nice.fr>
2021-04-21 10:14:23 +0200

    last

---
+++ libft/ft_strlcpy.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rfkaier <rfkaier@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/14 11:44:14 by rfkaier           #+#    #+#             */
+/*   Updated: 2021/04/14 11:45:36 by rfkaier          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dest, const char *src, size_t destsize)
+{
+	size_t	i;
+	size_t	src_len;
+
+	i = 0;
+	src_len = 0;
+	if (!dest)
+		return (0);
+	while (src[src_len])
+		src_len++;
+	if (!destsize)
+		return (src_len);
+	while (src[i] && i < destsize - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (src_len);
+}
